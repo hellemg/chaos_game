482f83e22498b8d5832ec65ec481848c4b290460
Helle Gråberg <helle.graaberg@hotmail.com>
2019-05-17 13:51:19 +0200

    Refactor chaosGame

--- canvas.py
+++ canvas.py
@@ -8,9 +8,9 @@ from globalConstants import *
 def setup_canvas(bg=(0, 57, 67), filename='Canvas.png'):
     """
     Creates an empty canvas
-    :param bg: tuple, 1x3. Backgroundcolor for the canvas
-    :param filename: string. Name of file that the canvas is saved to
-    :return: PIL Image Image
+    :param bg: tuple, 1 x 3; Backgroundcolor for the canvas
+    :param filename: string; Name of file that the canvas is saved to
+    :return: PIL Image, DIMS x DIMS
     """
     canvas = np.ones((DIMS, DIMS))
     canvas = Image.fromarray(canvas).convert("RGB")
@@ -18,20 +18,22 @@ def setup_canvas(bg=(0, 57, 67), filename='Canvas.png'):
         for c in range(DIMS):
             canvas.putpixel((r, c), bg)
     plt.imsave(filename, canvas)
+    print('..saved', filename)
     return canvas
 
 
-def setup_points(nr_of_points=5):
+def setup_points(nr_of_starting_points=5):
     """
     Adds evenly distributed starting points to canvas
-    :param nr_of_points: int. Number of points to add
-    :return: numpy array, nr_of_points x 2. Contains coordinates for the starting points
+    :param nr_of_points: int; Number of points to add
+    :return: numpy array, nr_of_points x 2; Contains coordinates for the starting points
     """
-    points_array = np.ones((nr_of_points, 2))
+    points_array = np.ones((nr_of_starting_points, 2))
     radius = DIMS / 2
-    theta = 2 * np.pi / nr_of_points
-    for i in range(nr_of_points):
+    theta = 2 * np.pi / nr_of_starting_points
+    for i in range(nr_of_starting_points):
         x = radius * np.cos(theta * i) + DIMS / 2
         y = radius * np.sin(theta * i) + DIMS / 2
         points_array[i] = x, y
-        return points_array
+    print('..calculated coordinates for starting points')
+    return points_array
--- chaosGame.py
+++ chaosGame.py
@@ -1,21 +1,20 @@
-from PIL import Image
-from matplotlib import pyplot as plt
 import numpy as np
 
 from globalConstants import *
 
 
-def chaos_game(canvas, points, mode='dark', cm_im=None):
+def chaos_game(canvas, points, bg=(0, 57, 67), filename='ChaosGame.png', colormap_image=None):
     """
     Creates pattern by chaos game algorithm
     :param canvas: PIL Image; empty canvas to fill with chaos-pattern
     :param points: Numpy array; points that give position of vertices
-    :param cm_im: PIL Image; color to add to chaos-pattern
-    :return:
+    :param bg: tuple, 1x3. Backgroundcolor for the canvas
+    :param filename: string. Name of file that the image is saved to
+    :param colormap_image: PIL Image; colored image to overlay pattern
+    :return: PIL Image, DIMS x DIMS; Image with colored chaos pattern
     """
-    # Set starting vertex
+    # Set initial conditions
     nr_of_points = points.shape[0]
-    filename = 'ChaosImageBW_' + mode + str(nr_of_points)
     current_point = points[0]
     current_x = DIMS / 3
     current_y = DIMS / 3
@@ -23,22 +22,21 @@ def chaos_game(canvas, points, mode='dark', cm_im=None):
         if (n % 1000 == 0):
             print("round: ", n)
         next_point = points[np.random.randint(nr_of_points)]
+        # Run chaos algorithm for 5 starting points
         if nr_of_points == 5:
             if (next_point[0] != current_point[0]) and (next_point[1] != current_point[1]):
                 current_x = int((current_x + next_point[0]) / 2)
                 current_y = int((current_y + next_point[1]) / 2)
                 current_point = next_point
-                # pixel = cm_im.getpixel((current_x, current_y))
-                pixel = 0
+                pixel = colormap_image.getpixel((current_x, current_y))
                 canvas.putpixel((current_x, current_y), pixel)
-        elif nr_of_points != 5:
+        # Run chaos algorithm for 3 starting points
+        elif nr_of_points == 3:
             current_x = int((current_x + next_point[0]) / 2)
             current_y = int((current_y + next_point[1]) / 2)
             current_point = next_point
-            # pixel = cm_im.getpixel((current_x, current_y))
-            pixel = 0
+            pixel = colormap_image.getpixel((current_x, current_y))
             canvas.putpixel((current_x, current_y), pixel)
-    canvas.save(filename+'.png')
-    print('Saved', filename+'.png')
-    #plt.imsave(filename, canvas)
-    # return canvas
+    canvas.save(filename)
+    print('..saved', filename)
+    return canvas
