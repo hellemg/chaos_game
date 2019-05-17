39e01e4c07cf1dcbe5a045a2c6d1a49f5b8dc394
Helle Gråberg <helle.graaberg@hotmail.com>
2019-05-17 13:39:51 +0200

    Refactor canvas.py

--- canvas.py
+++ canvas.py
@@ -5,18 +5,13 @@ from matplotlib import pyplot as plt
 from globalConstants import *
 
 
-def setup_canvas(mode='dark'):
+def setup_canvas(bg=(0, 57, 67), filename='Canvas.png'):
     """
-
-    :param mode:
-    :return:
+    Creates an empty canvas
+    :param bg: tuple, 1x3. Backgroundcolor for the canvas
+    :param filename: string. Name of file that the canvas is saved to
+    :return: PIL Image Image
     """
-    if mode == 'dark':
-        bg = (0, 57, 67)
-        filename = 'CanvasDark.png'
-    else:
-        bg = (0, 101, 129)
-        filename = 'CanvasMedium.png'
     canvas = np.ones((DIMS, DIMS))
     canvas = Image.fromarray(canvas).convert("RGB")
     for r in range(DIMS):
@@ -27,45 +22,16 @@ def setup_canvas(mode='dark'):
 
 
 def setup_points(nr_of_points=5):
-    # Create starting points
-    points_array = np.ones((5, 2))
-    radius = DIMS / 2
-    theta = 2 * np.pi / nr_of_points
-    for i in range(nr_of_points):
-        x = radius * np.cos(theta * i) + DIMS / 2
-        y = radius * np.sin(theta * i) + DIMS / 2
-        # To draw the points (make_circle is currently commented out)
-        # canvas = make_circle(canvas, x, y, 20, 150)
-        points_array[i] = x, y
-    return points_array
-
-
-def setup_canvas3D(mode='dark'):
-    # Large canvas
-    if mode == 'dark':
-        bg = (255, 255, 255)
-        filename = 'CanvasDark.png'
-    else:
-        bg = (0, 101, 129)
-        filename = 'CanvasMedium.png'
-    canvas = np.ones((DIMS, DIMS))
-    canvas = Image.fromarray(canvas).convert("RGB")
-    for r in range(DIMS):
-        for c in range(DIMS):
-            canvas.putpixel((r, c), bg)
-    plt.imsave(filename, canvas)
-    return canvas
-
-
-def setup_points3D(nr_of_points=3):
-    # Create starting points
+    """
+    Adds evenly distributed starting points to canvas
+    :param nr_of_points: int. Number of points to add
+    :return: numpy array, nr_of_points x 2. Contains coordinates for the starting points
+    """
     points_array = np.ones((nr_of_points, 2))
     radius = DIMS / 2
     theta = 2 * np.pi / nr_of_points
     for i in range(nr_of_points):
         x = radius * np.cos(theta * i) + DIMS / 2
         y = radius * np.sin(theta * i) + DIMS / 2
-        # To draw the points (make_circle is currently commented out)
-        # canvas = make_circle(canvas, x, y, 20, 150)
         points_array[i] = x, y
-    return points_array
+        return points_array
